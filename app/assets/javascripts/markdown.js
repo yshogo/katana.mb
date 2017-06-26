4a60bc5e8130f8171f7396520372bbb1ad7d3225
shogo.yamada <superman199323@gmail.com>
2017-06-27 07:51:55 +0900

    JavaScriptの確認用consoleの追加

--- app/assets/javascripts/markdown.js
+++ app/assets/javascripts/markdown.js
@@ -1,13 +1,13 @@
 $(function(){
 
-    $("#test").sideNav();
+    console.log("readed JavaScript!");
 
     $('#markdown').keyup(function () {
         var sync = function(){
-
             var user_write = $('#markdown').val();
             var md = marked(user_write);
             $('#preview').html(md);
+            console.log("push key!!");
         };
 
         sync();
