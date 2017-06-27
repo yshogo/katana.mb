efd408b61293343c3115bf2b891723352c6812c9
shogo.yamada <superman199323@gmail.com>
2017-06-27 21:38:48 +0900

    markdownを最初から読み込ませるように修正

--- app/assets/javascripts/markdown.js
+++ app/assets/javascripts/markdown.js
@@ -1,15 +1,21 @@
 $(function(){
 
     console.log("readed JavaScript!");
+    sync();
 
     $('#markdown').keyup(function () {
-        var sync = function(){
-            var user_write = $('#markdown').val();
-            var md = marked(user_write);
-            $('#preview').html(md);
+            var update = function(){
+                sync()
             console.log("push key!!");
         };
 
-        sync();
-    })
+        update();
+    });
+
+    function sync(){
+        var user_write = $('#markdown').val();
+        var md = marked(user_write);
+        $('#preview').html(md);
+        console.log("read markdown");
+    }
 });
