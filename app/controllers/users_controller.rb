4b2040df552f4c397280563068f63c83a03f1e8b
shogo.yamada <superman199323@gmail.com>
2017-07-01 12:05:37 +0900

    セッションが残っていない場合、トップページに戻すように修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,20 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(session[:user_id])
-    @article_list = Article.where(:uid => @user.uid)
+
+    if logged_in?
+      @user = User.find(session[:user_id])
+      @article_list = Article.where(:uid => @user.uid)
+    else
+      redirect_to root_path
+    end
+
   end
 
   def edit
     @article = Article.new
+  end
+
+  def searchArticle
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to:'sessions#create'
   get 'logout', to:'sessions#destroy'
   get '/edit', to:'users#edit'
+  get '/search', to:'users#search'
   post '/articles/edit'
   resources :users
   resource :articles
