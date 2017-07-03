d9d8891ff137fff722c8a64e91adb81496151045
shogo.yamada <superman199323@gmail.com>
2017-07-03 12:01:47 +0900

    検索機能の追加 like検索、between検索を使用した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,14 @@ class UsersController < ApplicationController
     @article = Article.new
   end
 
-  def searchArticle
+  def search
+    @user = User.find(session[:user_id])
+    @article_list = Article.user_id(session[:user_id]).search_between(parce_date(params[:start_date][0]),parce_date(params[:end_date][0])).article_like(params[:free_word])
+    render 'users/show'
+  end
 
+  private
+  def parce_date(date)
+    Time.zone.parse(date)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,27 @@
 class Article < ApplicationRecord
 
   validates :title, presence:true
+
+  #ログインしているユーザーに紐づく記事を取得
+  scope :user_id, -> (id){
+    where(uid: id) if id.present?
+  }
+
+  #記事の部分一致
+  scope :article_like, -> (search_word) {
+    where('article like ?', "%#{search_word}%")
+  }
+
+  #日付検索
+  scope :search_between, -> (from, to) {
+    if from.present? && to.present?
+      where(updated_at: from..to)
+    elsif from.present?
+      where('updated_at >= ?', from)
+    else
+      where('updated_at <= ?', to)
+    end
+  }
+
+
 end
